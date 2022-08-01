131d3a2a72b31a005d3194be5a9e7329c0bd4cca
Kevin Weatherwalks <kevin.weatherwalks@gmail.com>
2022-07-31 19:39:02 -0700

    add route to deliver saved files

--- planner/app.py
+++ planner/app.py
@@ -4,7 +4,7 @@ import math
 import os
 
 import cairo
-from flask import Flask, render_template, send_file
+from flask import Flask, jsonify, render_template, send_file
 
 from .functions import FILENAME, PDF_PATH, create_pdf
 
@@ -21,13 +21,28 @@ def entry_point():
 @app.route("/createfile", methods=["POST"])
 def deliver_file():
 
-    create_pdf()
+    try:
+        create_pdf()
+        print("---- PDF CREATED SUCCESSFULLY! ----")
+    except:
+        print("---- PDF CREATION FAILED :( ----")
+        return render_template("index.html")
+
     return send_file(
         PDF_PATH + "/" + FILENAME + ".pdf",
         attachment_filename=FILENAME + ".pdf",
-        as_attachment=True,
+        # as_attachment=True,
         cache_timeout=0,
     )
+
+
+# Fetch PDF results
+@app.route("/files/<string:name>")
+def files(name: str):
+    return send_file(
+        f"{PDF_PATH}/{name}.pdf", attachment_filename=f"{name}.pdf", cache_timeout=0
+    )
+    return jsonify(message=f"I want the file {filename}.pdf")
 
 
 # Run locally by executing app.py
